52c265cd94dcc85633e8207816ad70c4af8bf4c7
varma <sureshvarma@gmail.com>
2026-06-28 16:34:56 +0530

    Base code for my AutomationFramework

---
+++ src/test/java/com/api/tests/UserDeatailsAPITest.java
@@ -0,0 +1,51 @@
+package com.api.tests;
+import static io.restassured.RestAssured.*;
+
+import static io.restassured.http.ContentType.*;
+
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+
+
+import io.restassured.http.Header;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+
+
+
+public class UserDeatailsAPITest {
+	
+	
+	@Test
+	public void testUserDetailsAPI() {
+		// Rest Assured code to test User Details API
+		Header authHeader = new Header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3ODI2NDIyNTR9.bV8e_aQ_8caCCOwELkLRYh-Kx030snVoHaeTkO7TCrY");
+		given()
+			.baseUri("http://64.227.160.186:9000/v1")
+			.and()
+			.contentType(JSON)
+			.and()
+			.header(authHeader)
+			.and()
+			.accept(JSON)
+			.log().uri()
+			.log().method()
+			.log().headers()
+		.when()
+			.get("/userdetails")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.time(lessThan(1000L))
+			.and()
+			.body("message", equalTo("Success")).and()
+			.body(matchesJsonSchemaInClasspath("response-schema/UserDetailsResponceSchema.json"));
+		
+		
+		
+	}
+
+}
